d4b3c88473e6941288f8640ee3a1798b27637f15
aharrah95 <abigail.harrah@revature.net>
2022-03-29 14:08:12 -0500

    Project Dessert Python Practice, beginning; Cake subclass created

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ class Desserts:
         self.type = kind
         self.flavor = flavor
         self.price = price
-        # print("I'd like a " + "please.")
 
 
-        class Cake(Desserts):
-            pass
+class Cake(Desserts):
+            cake1 = Desserts('Cake', 'Black Forest Cake', '$30')
+            cake2 = Desserts('Cake', 'Red Velvet Cake', '$25')
+            cake3 = Desserts('Cake', 'Devils Food Cake', '$50')
+            cake4 = Desserts('Cake', 'Angel Food Cake', '$40')
+            cake5 = Desserts('Cake', 'Funfetti Cake', '$20')
 
-
-cake1 = Desserts('Cake', 'Black Forest Cake', 30)
-cake2 = Desserts('Cake', 'Red Velvet Cake', 25)
-cake3 = Desserts('Cake', 'Devils Food Cake', 50)
-cake4 = Desserts('Cake', 'Angel Food Cake', 40)
-cake5 = Desserts('Cake', 'Funfetti Cake', 20)
-
-print(cake2.flavor)
-print(cake3.price)
-print(cake5.type)
+            print("I'd love a slice of " + cake1.flavor + "!")
+            print("Certainly. That will be " + cake1.price + " dollars.")
